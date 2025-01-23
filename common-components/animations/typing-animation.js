51971b7bf5c5f3cde2d5cb12aa9b51f17ed0b30e
Adrian Busak <adrian.busak@edge-group.info>
2025-01-23 02:30:24 +0100

    typing-animation

---
+++ common-components/animations/typing-animation.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let typedElement = document.querySelector('.typeWriter-text');
+
+    let typewriter = new Typewriter(typedElement, {
+        loop: false
+    });
+
+    typewriter
+        .pauseFor(500)
+        .typeString('<span class="text">Budi izvrstan u onom što vidiš!</span>')
+        .pauseFor(300)
+        .deleteChars(6)
+        .typeString('<span class="text">voliš.</span>')
+        .pauseFor(300)
+        .typeString('</br><span class="text">ZAISKRI</span>')
+        .start();
+});
