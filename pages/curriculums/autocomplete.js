4f187249e3ad0190cef2d750b77b78eb4a230388
Adrian Busak <adrian.busak@edge-group.info>
2025-01-26 19:11:01 +0100

    autocomplete in curriculums

---
+++ pages/curriculums/autocomplete.js
@@ -0,0 +1,55 @@
+async function getAllCurriculums() {
+  const token = sessionStorage.getItem("jwtToken"); // Dohvati token iz localStorage
+  if (!token) {
+    console.error("Korisnik nije prijavljen.");
+    return;
+  }
+
+  const url = "https://www.fulek.com/data/api/supit/curriculum-list/hr";
+  try {
+    const data = await new Promise((resolve, reject) => {
+      $.ajax({
+        url: url,
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`, // Pass the token in the header
+        },
+        success: function (data) {
+          console.log("Svi kolegiji:", data.data);
+          resolve(data.data);
+        },
+        error: function (xhr, status, error) {
+          console.error("Greška prilikom dohvaćanja kolegija:", error);
+          reject(error);
+        },
+      });
+    });
+    return data;
+  } catch (error) {
+    console.error("Greška:", error);
+    throw error;
+  }
+}
+
+
+let availableTags = [];
+
+(async () => {
+  try {
+    const curriculums = await getAllCurriculums();
+    console.log("Dohvaćeni kolegiji:", curriculums);
+    curriculums.forEach(element => {
+      availableTags.push(element.kolegij);
+    });
+  } catch (error) {
+    console.error("Nije moguće dohvatiti kolegije:", error);
+  }
+})();
+
+console.log(availableTags);
+
+$( function() {
+  $( "#tags" ).autocomplete({
+    source: availableTags
+  });
+} );
