c3d0165b42c2ed821394481efc2dcb8479f72c9b
Adrian Busak <adrian.busak@edge-group.info>
2025-01-26 19:14:05 +0100

    deleting unnecessary things

--- common-components/contact/modal-contact.js
+++
@@ -1,8 +0,0 @@
-// export function addModule(){
-//     if($(document).ready(function(){
-//         $.get('/common-components/contact/contact-modal.html', (data) => {
-//             console.log(data);
-//             $('body').append(data);
-//         });
-//     }));
-// }
