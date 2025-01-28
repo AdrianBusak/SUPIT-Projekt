220ffbc85757e37018557ac019beba80edb44bbc
Adrian Busak <adrian.busak@edge-group.info>
2025-01-28 20:03:44 +0100

    switch button for log in and log out.

--- common-components/header/header.js
+++ common-components/header/header.js
@@ -17,6 +17,7 @@ function enableCurriculums() {
     const linkCurr = document.querySelector('.curriculums');
     if (linkCurr && linkCurr.classList.contains('disabled')) {
         linkCurr.classList.remove('disabled');
+        changeButtonLogIn();
     }
     
 
@@ -26,10 +27,24 @@ function disableCurriculums() {
     const linkCurr = document.querySelector('.curriculums');
     if (linkCurr && !linkCurr.classList.contains('disabled')) {
         linkCurr.classList.add('disabled');
+        changeButtonLogIn();
     }
 }
 
 function checkIfIsLogged() {
     const token = sessionStorage.getItem('jwtToken');
     return token;
+}
+
+
+function changeButtonLogIn(){
+    var logInButton = document.querySelector('.log-in');
+    const username = sessionStorage.getItem('username');
+    logInButton.querySelector('p').innerText = `Odjavi se | ${username}`;
+    logInButton.href = "/pages/home/components/dashboard-home.html";
+    if(checkIfIsLogged()){
+        logInButton.addEventListener("click", () => {
+            sessionStorage.removeItem('jwtToken');
+        });
+    }
 }
