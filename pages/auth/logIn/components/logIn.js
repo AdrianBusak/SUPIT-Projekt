5b22891fb1720ba974bcddec6cb6dae31ad3bf7f
Adrian Busak <adrian.busak@edge-group.info>
2025-01-29 23:33:12 +0100

    prevent login without any user data and added counting numbers on log in

--- pages/auth/logIn/components/logIn.js
+++ pages/auth/logIn/components/logIn.js
@@ -1,39 +1,41 @@
-const form = $('form');
-const username = $('#username');
-const password = $('#password');
+const form = $("form");
+const username = $("#username");
+const password = $("#password");
 
-const url = 'https://www.fulek.com/data/api/user/login';
+const url = "https://www.fulek.com/data/api/user/login";
 
-form.on('submit', function (event) {
+form.on("submit", function (event) {
   event.preventDefault();
 
   const userData = {
     username: username.val(),
-    password: password.val()
+    password: password.val(),
   };
-
-  $.ajax({
-    url: url,
-    method: 'POST',
-    contentType: 'application/json', // tip podataka koji se salje na server
-    data: JSON.stringify(userData),
-    dataType: 'json', //odgovor od servera
-    success: function (response) {
-      if (response.isSuccess) {
-        // Ako je prijava uspješna
-        sessionStorage.setItem('username', userData.username);
-        sessionStorage.setItem('jwtToken', response.data.token);
-        setTimeout(() => { // jos dodat trebam 1,2,3 div
-            location.replace('/pages/home/components/dashboard-home.html');
-        }, 3000);
-      } else {
-        alert('Pogrešno upisano korisničko ime ili lozinka.');
-      }
-    },
-    error: function (xhr, status, error) {
-      // Ako dođe do greške tijekom zahtjeva
-      console.error('Greška:', error);
-      alert('Došlo je do greške prilikom povezivanja sa serverom.');
-    }
-  });
+  if (userData.username && password) {
+    $.ajax({
+      url: url,
+      method: "POST",
+      contentType: "application/json", // tip podataka koji se salje na server
+      data: JSON.stringify(userData),
+      dataType: "json", //odgovor od servera
+      success: function (response) {
+        if (response.isSuccess) {
+          // Ako je prijava uspješna
+          sessionStorage.setItem("username", userData.username);
+          sessionStorage.setItem("jwtToken", response.data.token);
+          $('<div class="counting">1, 2, 3...</div>').insertBefore('#submit');
+          setTimeout(() => {
+            location.replace("/pages/home/components/dashboard-home.html");
+          }, 3000);
+        } else {
+          alert("Pogrešno upisano korisničko ime ili lozinka.");
+        }
+      },
+      error: function (xhr, status, error) {
+        // Ako dođe do greške tijekom zahtjeva
+        console.error("Greška:", error);
+        alert("Došlo je do greške prilikom povezivanja sa serverom.");
+      },
+    });
+  }
 });
