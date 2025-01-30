5b6f4b824230d8dc32c5a2cc7a883e150dd6f7e6
Adrian Busak <adrian.busak@edge-group.info>
2025-01-30 15:50:29 +0100

    newsletter

--- common-components/contact/contact-modal.js
+++ common-components/contact/contact-modal.js
@@ -1,29 +1,11 @@
-// $(document).ready(() => {
-//   $("#form-modal").on("click", (event) => {
-//     event.preventDefault();
+const btn = document.querySelector("#form-modal");
 
-//     const form = $('#form-modal');
-
-//     const notification = $('#ReceiveNewsletter').prop('checked') ? "true" : "false";
-//     console.log(notification);
-
-//     let formData = form.serializeArray();
-//     formData.pop();
-//     formData.push({ name: "ReceiveNewsletter", value: notification });
-//     console.log(formData);
-
-//     $.ajax({
-//       url: "https://www.fulek.com/mvc/supit/project-contact-form",
-//       method: "POST",
-//       contentType: "application/json", // tip podataka koji se salje na server
-//       data: JSON.stringify(formData),
-//       success: (response) => {
-//         console.log("Uspješno poslano:", response);
-//       },
-//       error: (error) => {
-//         console.log("Greška:", error);
-//       }
-//     });
-    
-//   });
-// });
+btn.addEventListener("submit",()=>{
+    const checkBox = document.querySelector("#ReceiveNewsletter");
+    if(checkBox.checked){
+        checkBox.value = "true";
+    }
+    else{
+        checkBox.value = "false";
+    }
+});
